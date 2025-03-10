f396b19c9da40b6ef7faecf3be41a881529e5deb
the-bug-lover <aleksander24sfj@gmail.com>
2025-03-10 07:47:16 +0100

    removed localhost

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -27,7 +27,7 @@ export default function ContactForm() {
     setResponseMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
